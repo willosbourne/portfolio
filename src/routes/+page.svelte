7c2cb29ba4b71454893d05fb45a87cdd439ee43b
Will Osbourne <will@osbourne.org>
2023-08-01 21:26:31 -0400

    updates to excessive word art logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     export const prerender = true;
 </script>
 
-<h1 class="font-mono text-3xl">Hey<br />It's Will Osbourne</h1>
+<h1 class="font-mono text-3xl">Hey<br />It's <span class="text-accent">Will Osbourne</span></h1>
 <p>
     I'm still working on this site,
     visit <a class="text-blue-600" href="https://willo.dev">willo.dev</a> to see a casual site,
